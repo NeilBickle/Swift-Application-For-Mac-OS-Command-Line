698920fb6c8761178d0a538c92b7801383b9ab30
Neil Bickle (AMBCS) (AssocIScT) (TMIET) (ICTTech) <43986316+NeilBickle@users.noreply.github.com>
2020-01-28 09:28:45 +0000

    Fixed error with project file

---
+++ SwiftTest/SwiftTest/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SwiftTest
+//
+
+
+
+import Foundation
+
+let Input = NSFullUserName()
+print("Welcome " + Input)
+var HostVersion = ProcessInfo.processInfo.hostName
+print("The host name is ", HostVersion)
+var OSVersion = ProcessInfo.processInfo.operatingSystemVersion
+print("The operating system version is ", OSVersion)
+
+
